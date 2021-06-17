240ed08480af94a383b10a2dcd031ad4894ec3d9
marclachartre <marc.lachartre@gmail.com>
2021-06-17 17:07:57 +0200

    react-history added

--- app/javascript/packs/user_account.jsx
+++ app/javascript/packs/user_account.jsx
@@ -1,8 +1,8 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route, BrowserHistory, Switch, Link } from 'react-router-dom'
-
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import { BrowserHistory } from 'react-history'
 
 import {Details} from '../components/react_user_account/details'
 import {MyReviews} from '../components/react_user_account/my_reviews'
